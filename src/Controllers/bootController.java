4a424a6ce70318caacb6e1115084a3312f89544d
shahbajsingh <shahbajs987@gmail.com>
2021-12-04 19:50:54 -0500

    cleaning up bootController code

--- src/Controllers/bootController.java
+++ src/Controllers/bootController.java
@@ -23,175 +23,195 @@ import java.util.Date;
 
 import static java.lang.Thread.sleep;
 
-public class bootController {
-
-    protected String data[] = new String[6];
-
-    @FXML
-    private AnchorPane sendPane;
-
-
-    @FXML
-    private JFXButton receive;
-
-    @FXML
-    private AnchorPane receivePane;
-
-    @FXML
-    private JFXTextArea receiveTextArea;
-
-    @FXML
-    private JFXTextField idKey;
-
-    @FXML
-    private Label warning;
-
-    @FXML
-    private JFXTextField privKey,sendMessageID;
-
-    @FXML
-    private JFXPasswordField sendPrivateKey;
-
-
-    @FXML
-    private JFXButton submitReceive;
-
-    @FXML
-    private JFXButton setting;
-
-    @FXML
-    private JFXButton send;
-
-    @FXML
-    private JFXTextArea message;
-
-    @FXML
-    private JFXButton submit;
-
-    @FXML
-    private JFXButton close;
-
-    @FXML
-    private JFXButton clear;
-
-    Parent root;
-    Scene scene;
-    Stage stage;
-
-
-    @FXML
-    void handleClear(ActionEvent event)
+public class bootController
     {
-        if(event.getSource().equals(clear))
+
+        protected String data[] = new String[6];    // For each entry into Postgre database
+
+        @FXML
+        private AnchorPane sendPane;        // Defaulted to front
+
+        @FXML
+        private AnchorPane receivePane;
+
+        @FXML
+        private JFXButton send;             // Button for sendPane
+
+        @FXML
+        private JFXButton receive;          // Button for receivePane
+
+        @FXML
+        private Label warning;          // Activated with empty input bodies
+
+        @FXML
+        private Label success;          // Activated when outgoing message is successfully processed
+
+        @FXML
+        private JFXButton close;
+
+        @FXML
+        private JFXButton clear;
+
+        @FXML
+        private JFXTextArea messageInput;
+
+        @FXML
+        private JFXButton settings;
+
+        @FXML
+        private JFXButton sendSubmit;
+
+        @FXML
+        private JFXPasswordField sendPrivateKey;
+
+
+        @FXML
+        private JFXTextArea receiveTextArea;
+
+        @FXML
+        private JFXTextField idKey;
+
+
+        @FXML
+        private JFXTextField privKey, sendMessageID;
+
+
+        @FXML
+        private JFXButton receiveSubmit;
+
+
+        Parent root;
+        Scene scene;
+        Stage stage;
+
+
+        @FXML
+        void handleSend(ActionEvent event)      // Clicking send button pushes send pane
         {
-           message.setText("");
-        }
-    }
-
-    @FXML
-    void handleSubmitReceive(ActionEvent event) {
-
-    }
-
-
-
-    @FXML
-    void handleSettings(ActionEvent event) throws IOException {
-
-        if(event.getSource().equals(setting)){
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(Constants.SETTING));
-            root = loader.load();
-
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+            if (event.getSource().equals(send))
+            {
+                receivePane.toBack();
+                sendPane.toFront();
+            }
 
         }
 
-    }
 
-
-    @FXML
-    void handleClose(ActionEvent event) {
-        if(event.getSource().equals(close)){
-            System.exit(0);
-
-        }
-    }
-
-    @FXML
-    void handleReceive(ActionEvent event) {
-        if(event.getSource().equals(receive)){
-            sendPane.toBack();
-            receivePane.toFront();
-
-        }
-
-    }
-
-    @FXML
-    void handleSend(ActionEvent event) {
-        if (event.getSource().equals(send)) {
-            receivePane.toBack();
-
-            sendPane.toFront();
-
-
-
-        }
-
-    }
-
-    @FXML
-    void handleSubmit(ActionEvent event) throws UnknownHostException, IOException, InterruptedException
-    {
-        warning.setText("");
-
-        if(!sendPrivateKey.getText().equals("") && !sendMessageID.getText().equals("") && !message.getText().equals(""))
+        @FXML
+        void handleReceive(ActionEvent event)   // Clicking receive button pushes receive pane
         {
-            printData(getData());
-            message.setText("Message has successfully sent");
+            if (event.getSource().equals(receive))
+            {
+                sendPane.toBack();
+                receivePane.toFront();
+            }
+
         }
 
-        else
+        @FXML
+        void handleClose(ActionEvent event)     // Close button exits program from application
         {
-            warning.setText("Fields can not be empty");
+            if (event.getSource().equals(close))
+            {
+                System.exit(0);
+            }
+        }
+
+        @FXML
+        void handleSettings(ActionEvent event) throws IOException   // Boots new settings window
+        {
+
+            if (event.getSource().equals(settings))
+            {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(Constants.SETTING));
+                root = loader.load();
+
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            }
+
         }
 
 
 
-        sendMessageID.setText("");
-        sendPrivateKey.setText("");
-
-    }
-
-    private void sleep() throws InterruptedException
-    {
-        Thread.sleep(2000);
-    }
+        // SEND PANE
 
 
+        @FXML
+        void handleClear(ActionEvent event)
+        {
+            if (event.getSource().equals(clear))
+            {
+                messageInput.setText("");
+            }
+        }
 
-    private String [] getData() throws IOException {
-        data[0] = sendMessageID.getText();
-        data[1] =  message.getText();
-        data[2] = sendPrivateKey.getText();
-        data[3] = java.util.Calendar.getInstance().getTime().toString();
-        data[4] = InetAddress.getLocalHost().getHostAddress();
-        data[5] = System.getProperty("os.name");
-        return data;
-
-    }
+        @FXML
+        void handleSendSubmit(ActionEvent event) throws IOException
+        {
+            if (event.getSource().equals(sendSubmit))
+            {
+                warning.setText("");
+                success.setText("");
 
 
-    @SuppressWarnings("unused")
-    private void printData(String[] data) {
-        for(int i = 0; i < data.length; i++){
-            System.out.println("\n" + data[i]);
+                if (!sendPrivateKey.getText().equals("")
+                        && !sendMessageID.getText().equals("")
+                        && !messageInput.getText().equals(""))
+                {
+                    printData(getData());
+                    success.setText("Message sent!");
+                } else
+                {
+                    warning.setText("Fields can not be empty");
+                }
+
+                messageInput.setText("");
+                sendMessageID.setText("");
+                sendPrivateKey.setText("");
+            }
 
         }
 
-    }
 
-}
+        // RECEIVE PANE
+
+
+        @FXML
+        void handleReceiveSubmit(ActionEvent event)
+        {
+            if(event.getSource().equals(receiveSubmit))
+            {
+
+            }
+
+        }
+
+
+        private String[] getData() throws IOException
+        {
+            data[0] = sendMessageID.getText();
+            data[1] = messageInput.getText();
+            data[2] = sendPrivateKey.getText();
+            data[3] = java.util.Calendar.getInstance().getTime().toString();
+            data[4] = InetAddress.getLocalHost().getHostAddress();
+            data[5] = System.getProperty("os.name");
+            return data;
+
+        }
+
+
+        @SuppressWarnings("unused")
+        private void printData(String[] data)
+        {
+            for (int i = 0; i < data.length; i++)
+            {
+                System.out.println("\n" + data[i]);
+
+            }
+        }
+
+    }
